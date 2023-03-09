c6296b17717fdf609fcf4189bb3c1a6068de8c27
nedvelkov <69073418+nedvelkov@users.noreply.github.com>
2023-03-09 18:21:34 +0200

    Add main file with user input

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Go lang")
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = conferenceTickets
+
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets you want to buy")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a conformation email at %v \n", firstName, lastName, userTickets, email)
 }
