edeef11fb3fe4089e79d960849e7cb9a20b96c5d
nedvelkov <69073418+nedvelkov@users.noreply.github.com>
2023-03-11 18:02:55 +0200

    update main.go

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -11,10 +11,11 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var booking []string
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -28,5 +29,12 @@ func main() {
 	fmt.Println("Enter number of tickets you want to buy")
 	fmt.Scan(&userTickets)
 
+	remainingTickets -= userTickets
+	booking = append(booking, firstName+" "+lastName)
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a conformation email at %v \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n", booking)
+
 }
