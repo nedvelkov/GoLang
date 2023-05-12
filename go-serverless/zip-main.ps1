f5172e1aa6d12e462066a1236b3079e05ab0e0e8
NedyalkoVelkov <nvelkov@nemetschek.bg>
2023-05-12 11:05:28 +0300

    update zip-main.ps1 due grammar mistake

--- go-serverless/zip-main.ps1
+++ go-serverless/zip-main.ps1
@@ -2,4 +2,5 @@ $env:GOOS = "linux"
 $env:GOARCH = "amd64"
 $env:CGO_ENABLED = "0"
 go build -o main main.go
-~\Go\Bin\build-lambda-zip.exe -o main.zip bootstrap
+~\Go\Bin\build-lambda-zip.exe -o main.zip main
+
