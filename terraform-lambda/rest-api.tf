eab61270563328d02baeeb0b6911ac0c4862c026
NedyalkoVelkov <nvelkov@nemetschek.bg>
2023-05-12 15:46:49 +0300

    update terraform - rest api integration is now PROXY and request to lambda contains request data

--- terraform-lambda/rest-api.tf
+++ terraform-lambda/rest-api.tf
@@ -1,5 +1,29 @@
 resource "aws_api_gateway_rest_api" "rest" {
-  name = "AWS Lambda"
+  body = jsonencode({
+    openapi = "3.0.1"
+    info = {
+      title   = "example"
+      version = "1.0"
+    }
+    paths = {
+      "/path1" = {
+        get = {
+          x-amazon-apigateway-integration = {
+            httpMethod           = "GET"
+            payloadFormatVersion = "1.0"
+            type                 = "HTTP_PROXY"
+            uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+          }
+        }
+      }
+    }
+  })
+
+  name = "example"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 resource "aws_api_gateway_resource" "resource" {
@@ -16,17 +40,14 @@ resource "aws_api_gateway_method" "method" {
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  type                    = "AWS"
   rest_api_id             = aws_api_gateway_rest_api.rest.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
+  type                    = "AWS_PROXY"
 
   uri = aws_lambda_function.hello_world.invoke_arn
 
-  request_templates = {
-    "application/json" = "#set($allParams = $input.params())\n{\n\"body-json\" : $input.json(\"$\"),\n\"params\" : {\n#foreach($type in $allParams.keySet())\n    #set($params = $allParams.get($type))\n\"$type\" : {\n    #foreach($paramName in $params.keySet())\n    \"$paramName\" : \"$util.escapeJavaScript($params.get($paramName))\"\n        #if($foreach.hasNext),#end\n    #end\n}\n    #if($foreach.hasNext),#end\n#end\n},\n\"stage-variables\" : {\n#foreach($key in $stageVariables.keySet())\n\"$key\" : \"$util.escapeJavaScript($stageVariables.get($key))\"\n    #if($foreach.hasNext),#end\n#end\n},\n\"context\" : {\n    \"api-id\" : \"$context.apiId\",\n    \"api-key\" : \"$context.identity.apiKey\",\n    \"http-method\" : \"$context.httpMethod\",\n    \"stage\" : \"$context.stage\",\n    \"source-ip\" : \"$context.identity.sourceIp\",\n    \"user-agent\" : \"$context.identity.userAgent\",\n    \"request-id\" : \"$context.requestId\",\n    \"resource-id\" : \"$context.resourceId\",\n    \"resource-path\" : \"$context.resourcePath\"\n    }\n}\n"
-  }
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
@@ -42,16 +63,17 @@ resource "aws_api_gateway_integration_response" "integration_response_200" {
   http_method = aws_api_gateway_method.method.http_method
   status_code = aws_api_gateway_method_response.response_200.status_code
 
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" : "'*'",
-    "method.response.header.Content-Type" : "'text/html'"
+  response_templates = {
+    "application/xml" = <<EOF
+      #set($inputRoot = $input.path('$'))
+      <?xml version="1.0" encoding="UTF-8"?>
+      <message>
+          $inputRoot.body
+      </message>
+      EOF
   }
 
    depends_on = [
     aws_api_gateway_integration.integration
   ]
-
-  response_templates = {
-    "text/html" : "$input.path('$')"
-  }
 }
