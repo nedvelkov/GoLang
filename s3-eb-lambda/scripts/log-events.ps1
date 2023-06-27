8c6121554dc2eaf40a353e6f2de80a11469b793a
NedyalkoVelkov <nvelkov@nemetschek.bg>
2023-06-27 09:58:57 +0300

    update s3 project - script for logs takes as parameter lambdaName and logsCount. Logs count is optional.

--- s3-eb-lambda/scripts/log-events.ps1
+++ s3-eb-lambda/scripts/log-events.ps1
@@ -1,26 +1,43 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$lambdaName,
+    [int]$logsCount = 5
+)
+
+
 # Run AWS CLI command and capture the JSON response
-$jsonResponse = & awslocal logs describe-log-streams --log-group-name /aws/lambda/go-lambda --order-by LastEventTime --descending --limit 1 | Out-String
+$jsonResponse = & awslocal logs describe-log-streams --log-group-name /aws/lambda/$lambdaName --order-by LastEventTime --descending --limit $logsCount | Out-String
 
 # Convert JSON to PowerShell object
 $result = ConvertFrom-Json $jsonResponse
 
-# Extract values
-$logStream = $result.logStreams[0].logStreamName
+# Extract log streams from the JSON response
+$logStreams = $result.logStreams
 
+# Loop through each log stream and retrieve log events
+foreach ($logStream in $logStreams) {
+    $logStreamName = $logStream.logStreamName
+    $logTime = (Get-Date 01.01.1970).AddSeconds($logStream.creationTime / 1000)  
+    Write-Output "Log time: $($logTime)"
+    Write-Output "Events:"
 
-# Write-Host $logStream
+    # Run AWS CLI command to get log events for the log stream
+    $jsonEventsResponse = & awslocal logs get-log-events --log-group-name /aws/lambda/$lambdaName --log-stream-name $logStreamName | Out-String
 
+    # Convert JSON to PowerShell object
+    $eventsResult = ConvertFrom-Json $jsonEventsResponse
 
-$jsonResponse = & awslocal logs get-log-events --log-group-name /aws/lambda/go-lambda --log-stream-name $logStream | Out-String
-# Convert JSON to PowerShell object
-$result = ConvertFrom-Json $jsonResponse
+    # Extract events from the JSON response
+    $events = $eventsResult.events
 
-# Extract events from the JSON response
-$events = $result.events
+    # Loop through each event and display the desired output format
+    foreach ($event in $events) {
+        $timestamp = $event.timestamp / 1000
+        $date2 = (Get-Date 01.01.1970).AddSeconds($timestamp)  
+        $message = $event.message
+    
+        Write-Host ("`t{0} : {1}" -f $date2, $message )
+    }
 
-# Loop through each event and display the desired output format
-foreach ($event in $events) {
-    $message = $event.message
-
-    Write-Host $message
+    Write-Output ""
 }
